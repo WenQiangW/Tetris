77ea99a93b3d39ce6686199d54ca6c266c0f1e5c
WenQiang <WenQiangW@users.noreply.github.com>
2017-06-08 10:27:38 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,6 +1,6 @@
 #include"Head.h"
 #define _CRT_SECURE_NO_WARNINGS 1
-1 23
+1 234
 bool MoveAble(int rockIndex, RockLocation_t* currentLocatePtr, int f_direction);
 void SetGameBoardFlag(int rockIdx, RockLocation_t* curRockLocation);
 void UserHitKeyBoard(char userHit, int* RockIndex, RockLocation_t* curRockLocation);
